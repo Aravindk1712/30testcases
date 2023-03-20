c2cc0712e41a6896624cc38bf7173ff32bc73632
Aravindk1712 <aravind.k1712@gmail.com>
2023-03-21 02:07:24 +0530

    30 testcases added

---
+++ src/test/java/runner/RunClass.java
@@ -0,0 +1,23 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src\\test\\resources\\FeatureFile",
+				
+				dryRun = false ,
+		glue= {"StepDefinition"},
+		plugin= {"json:target/cucumber.json"})
+//		glue= {"StepDefinition"},
+//		plugin = { "html:target/cucumber-reports/Cucumber.html","json:target/cucumber.json"},
+//		monochrome = true
+//		)
+	
+public class RunClass {
+
+}
+//tags= "@test22 ",tags= "@test3 ",
